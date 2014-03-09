5bfbcfc3ae49830a6eea7933766ed51c0927b6db
Sam Koblenski <koblenski@gmail.com>
2014-03-08 22:17:49 -0600

    initial commit with SqlTemplate scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Templater::Application.config.secret_key_base = 'f85111d305d0e41daf06c93149e754058a5ec8f214a04d3352c1d6a9f751d16624658a1f32bbf19499f1574c875c23fa4e0a4fa9e1cc5db52d32f7a5518da797'
