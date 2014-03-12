3e07d6054c355ed56d8814eae0a757ee3572c598
Sam Koblenski <koblenski@gmail.com>
2014-03-11 22:23:25 -0500

    add CMS controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Templater::Application.routes.draw do
+  resources :users
+
   resources :sql_templates
+
+  get "cms/*page", to: "cms#respond"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
